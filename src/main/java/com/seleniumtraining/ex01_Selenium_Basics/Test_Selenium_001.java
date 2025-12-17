31756af9b0ee8b919bca00ea1a98d147ffa9764f
Shafna Shamsudeen <shafnaalesha@gmail.com>
2025-12-17 11:50:13 +0530

    commit1

---
+++ src/main/java/com/seleniumtraining/ex01_Selenium_Basics/Test_Selenium_001.java
@@ -0,0 +1,7 @@
+package com.seleniumtraining.ex01_Selenium_Basics;
+
+public class Test_Selenium_001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
